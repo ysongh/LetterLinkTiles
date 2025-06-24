22403e6d6fbe7904ee7c8e812755cdc3e7c705d2
You Song Hou <ysongweb3@gmail.com>
2025-06-23 20:37:19 -0400

    feat: add a duplicate AI agent to support flow network

--- react/src/wagmi.ts
+++ react/src/wagmi.ts
@@ -1,14 +1,15 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { http, createConfig } from "wagmi";
-import { base, mainnet, hardhat } from "wagmi/chains";
+import { base, mainnet, hardhat, flowTestnet } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [base, mainnet, hardhat],
+  chains: [base, mainnet, hardhat, flowTestnet],
   connectors: [farcasterFrame()],
   transports: {
     [base.id]: http(),
     [mainnet.id]: http(),
     [hardhat.id]: http(),
+    [flowTestnet.id]: http(),
   },
 });
 
