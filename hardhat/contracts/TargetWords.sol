0aa15ce3d8dc606e22517222fdb9fff6544a8975
You Song Hou <ysongweb3@gmail.com>
2025-09-07 21:26:40 -0400

    feat: add function to calculate word score

--- hardhat/contracts/TargetWords.sol
+++ hardhat/contracts/TargetWords.sol
@@ -35,6 +35,9 @@ contract TargetWords {
     26 // Z (1 tile)
   ];
 
+  // Tile scores (A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10)
+  uint8[27] private tileScores = [0, 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
+
   // Structs
   struct Player {
     bool isActive;
@@ -92,5 +95,14 @@ contract TargetWords {
     ))) % tilePool.length;
     
     return tilePool[randomIndex];
+  }
+
+  // Calculate word score based on tile values
+  function calculateWordScore(uint8[] memory tilesUsed) internal view returns (uint256) {
+    uint256 score = 0;
+    for (uint i = 0; i < tilesUsed.length; i++) {
+      score += tileScores[tilesUsed[i]];
+    }
+    return score;
   }
 }
