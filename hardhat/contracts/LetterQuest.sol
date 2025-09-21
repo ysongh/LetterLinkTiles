207b31e2b8728a04d92de4540be8001841127575
You Song Hou <ysongweb3@gmail.com>
2025-09-21 00:42:32 -0400

    feat: add function to submit word

--- hardhat/contracts/LetterQuest.sol
+++ hardhat/contracts/LetterQuest.sol
@@ -6,6 +6,7 @@ contract LetterQuest {
   event TileMinted(address indexed player, uint8 tile);
   event TileDiscard(address indexed player, uint8[] tile);
   event RollResult(address player, uint8 num);
+  event WordSubmitted(address indexed player, string word);
 
   // Tile scores (A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10)
   uint8[27] private tileScores = [0, 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
@@ -56,6 +57,25 @@ contract LetterQuest {
     }
 
     emit RollResult(msg.sender, randomNumber);
+  }
+
+  function submitWord(uint8[] calldata tilesUsed) external {
+    if (_compareTilesToWord(tilesUsed, targetWord1)) {
+      uint256 wordScore = calculateWordScore(tilesUsed);
+      players[msg.sender].score += wordScore;
+      removeTilesFromPlayer(msg.sender, tilesUsed);
+      emit WordSubmitted(msg.sender, targetWord1);
+    } else if (_compareTilesToWord(tilesUsed, targetWord2)) {
+      uint256 wordScore = calculateWordScore(tilesUsed);
+      players[msg.sender].score += wordScore;
+      removeTilesFromPlayer(msg.sender, tilesUsed);
+      emit WordSubmitted(msg.sender, targetWord2);
+    } else if (_compareTilesToWord(tilesUsed, targetWord3)) {
+      uint256 wordScore = calculateWordScore(tilesUsed);
+      players[msg.sender].score += wordScore;
+      removeTilesFromPlayer(msg.sender, tilesUsed);
+      emit WordSubmitted(msg.sender, targetWord3);
+    }
   }
 
   function addTargetWords(string memory _targetWord1, string memory _targetWord2, string memory _targetWord3) external onlyOwner{
