1a5e846bc013c6670280e0ecac3375768668c43c
You Song Hou <ysongweb3@gmail.com>
2025-09-30 22:25:22 -0400

    feat: update tile scores

--- hardhat/contracts/LetterQuest.sol
+++ hardhat/contracts/LetterQuest.sol
@@ -8,8 +8,7 @@ contract LetterQuest {
   event RollResult(address player, uint8 num);
   event WordSubmitted(address indexed player, string word);
 
-  // Tile scores (A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10)
-  uint8[27] private tileScores = [0, 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
+  uint8[27] public tileScores = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
   struct Player {
     bool isActive;
